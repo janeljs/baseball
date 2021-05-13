1ed3ceb8ed2c8a10968764dc70933966dd5f094f
eve712 <62237639+eve712@users.noreply.github.com>
2021-05-14 01:43:12 +0900

    style: Main 컴포넌트 스타일 작성

--- fe/src/components/main/Teams.jsx
+++ fe/src/components/main/Teams.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { GlobalContext } from "../../App";
-import { getURL, deepCopy, requestPost } from "../../utils/util";
+import { getURL, requestPost } from "../../utils/util";
 
-const Teams = ({ teamSet }) => {
+const Teams = ({ teamSet, order }) => {
   const { dispatch } = useContext(GlobalContext);
 
   const setTeams = async (teamName, teamId, idx) => {
@@ -27,14 +28,59 @@ const Teams = ({ teamSet }) => {
   };
 
   return (
-    <div>
+    <TeamNameBox>
+      <GameNumber>GAME {order + 1}</GameNumber>
       {teamSet.map((team, i) => (
-        <Link to="/baseballGame" onClick={() => setTeams(team.name, team.id, i)}>
-          {team.name}
-        </Link>
+        <>
+          {i === 1 && <Vs>VS</Vs>}
+          <Link to="/baseballGame" style={{ textDecoration: "none" }} onClick={() => setTeams(team.name, team.id, i)}>
+            <TeamName>{team.name}</TeamName>
+          </Link>
+        </>
       ))}
-    </div>
+    </TeamNameBox>
   );
 };
 
 export default Teams;
+
+const TeamNameBox = styled.div`
+  position: relative;
+  padding: 45px 45px 20px;
+  border-radius: 20px;
+  margin-bottom: 30px;
+
+  background: #ddd;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const GameNumber = styled.span`
+  position: absolute;
+  top: 15px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 18px;
+  color: #eb4833;
+`;
+
+const TeamName = styled.div`
+  width: 140px;
+  font-size: 35px;
+  font-weight: 700;
+  color: #333;
+  text-align: center;
+  &:hover {
+    color: #eb4833;
+  }
+`;
+
+const Vs = styled.span`
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 30px;
+  font-weight: 900;
+  color: #666;
+`;
--- fe/src/components/shared/Title.jsx
+++ fe/src/components/shared/Title.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
 const Title = () => {
-  return <h1>BASEBALL GAME ONLINE</h1>;
+  return <Heading>BASEBALL GAME ONLINE</Heading>;
 };
 
 export default Title;
+
+const Heading = styled.h1`
+  padding: 40px;
+  font-size: 50px;
+  font-weight: 900;
+  color: #fff;
+  text-align: center;
+`;
